92e51d34e7ab9e3e7d4b92b3543f47270047cc88
David Patron Fernandez <tronez98@gmail.com>
2023-02-06 16:03:58 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.juntaelectoral {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.juntaelectoral to javafx.fxml;
+    exports com.example.juntaelectoral;
+}
